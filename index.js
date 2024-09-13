027cfa5098dd291803696854df5f0c5b3d22c87f
Donald <donumeh54@gmail.com>
2024-09-13 11:04:42 +0100

    updated render test

--- index.js
+++ index.js
@@ -1,177 +1,52 @@
-
+const Note = require("./models/note");
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'));
 
+
 const requestLogger = (request, response, next) => {
-    console.log('Method: ', request.method)
-    console.log('Path: ', request.path)
-    console.log('Body: ', request.body)
-    console.log('--')
-    next()
+    console.log('Method: ', request.method);
+    console.log('Path: ', request.path);
+    console.log('Body: ', request.body);
+    console.log('--');
+    next();
 }
 
 app.use(requestLogger);
 
 
-let notes = [
-    {
-      "id": "1",
-      "content": "HTML is easy",
-      "important": false
-    },
-    {
-      "id": "2",
-      "content": "Browser can execute only JavaScript",
-      "important": true
-    },
-    {
-      "id": "3",
-      "content": "GET and POST are the most important methods of HTTP protocol",
-      "important": false
-    },
-    {
-      "id": "4",
-      "content": "a new note...",
-      "important": false
-    },
-    {
-      "id": "5",
-      "content": "New one",
-      "important": false
-    },
-    {
-      "id": "6",
-      "content": "",
-      "important": false
-    },
-    {
-      "id": "7",
-      "content": "a new note...",
-      "important": false
-    },
-    {
-      "id": "8",
-      "content": "Hello World",
-      "important": false
-    },
-    {
-      "id": "9",
-      "content": "a new note...",
-      "important": true
-    },
-    {
-      "id": "10",
-      "content": "a new note...",
-      "important": true
-    },
-    {
-      "id": "11",
-      "content": "a new note...",
-      "important": true
-    },
-    {
-      "id": "12",
-      "content": "a new note...",
-      "important": true
-    },
-    {
-      "id": "13",
-      "content": "a new note...",
-      "important": true
-    },
-    {
-      "id": "14",
-      "content": "a new note...",
-      "important": false
-    },
-    {
-      "id": "15",
-      "content": "a new note...",
-      "important": false
-    },
-    {
-      "id": "16",
-      "content": "a new note...",
-      "important": false
-    },
-    {
-      "id": "17",
-      "content": "Somethinf",
-      "important": true
-    },
-    {
-      "id": "18",
-      "content": "a new note...",
-      "important": false
-    },
-    {
-      "id": "19",
-      "content": "a new note...",
-      "important": false
-    },
-    {
-      "id": "20",
-      "content": "a new note...",
-      "important": true
-    },
-    {
-      "id": "21",
-      "content": "a new note...",
-      "important": true
-    },
-    {
-      "id": "22",
-      "content": "a new note...",
-      "important": false
-    },
-    {
-      "id": "23",
-      "content": "isis",
-      "important": true
-    }
-  ];
-
-const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-
-    return String(maxId + 1)
-}
-
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>');
-})
+});
 
 app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
     response.json(notes);
-})
+  })
+});
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    const note = notes.find(note => note.id === id);
+app.get('/api/notes/:id', (request, response, next) => {
+   Note.findById(request.params.id).then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.statusMessage = "Note not found";
+        response.status(404).json({"error": "Note with id not found"})
+      }
+   }).catch(error => next(error))
+});
 
-    if (note) {
-        response.json(note);
-    }
-    else {
-        response.statusMessage = "Current note not found"
-        response.status(404).json({message: 'note does not exits'});
-    }
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    response.statusMessage = "Note deleted"
-    response.status(204).json({message: "note deleted successfully"})
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end();
+    })
+    .catch(error => next(error));
 })
 
 app.post('/api/notes', (request, response) => {
@@ -179,17 +54,32 @@ app.post('/api/notes', (request, response) => {
 
     if (!body.content) {
         response.statusMessage = "Content cannot be empty";
-        response.status(404).json({message: "404: Content cannot be empty"});
+        response.status(404).end();
     }
 
-    const note = {
+    const note = new Note ({
         content: body.content,
         important: Boolean(body.important) || false,
-        id: generateId(),
-    }
-    notes = notes.concat(note)
+    });
 
-    response.json(notes);
+    note.save().then(savedNote => {
+      response.json(savedNote);
+    })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -198,7 +88,19 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error);
+}
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
